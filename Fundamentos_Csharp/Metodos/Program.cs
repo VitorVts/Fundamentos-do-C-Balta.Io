ca58d68291c948db04c4111a41cb277222215138
VitorVts <vitorgguimaraes56@gmail.com>
2024-06-21 17:25:08 -0300

    Feito os exericicios prátitico referente a metodos e structs

---
+++ Fundamentos_Csharp/Metodos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MeuMetodo();
+
+            string nome = RetornaNome("Vitor","Gomes",25);
+            System.Console.WriteLine(nome);
+        }
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C# é maneiro");
+        }
+
+        static string RetornaNome(string nome, string sobrenome, int idade = 0)
+        {
+            return $"{nome} {sobrenome} {idade.ToString()}";
+        }
+    }
+}
